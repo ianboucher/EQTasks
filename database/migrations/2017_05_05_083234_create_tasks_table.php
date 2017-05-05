db5c38ca3d3ce74e0b850d528e043e3101f04e54
ianboucher <i.a.boucher@gmail.com>
2017-05-05 10:15:59 +0100

    Add example test, db seeds and routes

--- database/migrations/2017_05_05_083234_create_tasks_table.php
+++ database/migrations/2017_05_05_083234_create_tasks_table.php
@@ -18,7 +18,7 @@ class CreateTasksTable extends Migration
             $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('description');
-            $table->boolean('completed');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
---
+++ tests/Feature/TasksTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TasksTest extends TestCase
+{
+    /**
+    * A basic test example for tasks
+    *
+    * @return void
+    */
+    public function testNewTask()
+    {
+        $response = $this->json('POST', '/api/v1.0.0/users/1/tasks', [
+            'description' => 'A task from PHPUnit',
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'description' => 'A task from PHPUnit',
+            ]);
+    }
+}
