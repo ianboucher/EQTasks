5824def09e351b6d245e25eed3082d89dfbe9de0
ianboucher <i.a.boucher@gmail.com>
2017-05-05 20:15:09 +0100

    Extract navbar & minor styling improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,39 +13,9 @@
 </head>
 
 <body>
-    <div class="navbar-wrapper">
-        <nav class="navbar navbar-inverse navbar-static-top">
 
-                <div class="navbar-header">
-                    <a class="navbar-brand" ui-sref="landing">EquiniTasks</a>
-                </div>
-
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li><a ui-sref="pending">Pending Tasks</a></li>
-                        <li><a ui-sref="completed">Completed Tasks</a></li>
-                    </ul>
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a ui-sref="login">Login</a></li>
-                        <li><a ui-sref="signup">Sign-up</a></li>
-                        <li><a u-sref="#"></a></li>
-                        <li><a ng-click="logout()">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-
-    <div class="container">
-        <div class="row row-centered">
-            <div class="col-xs-10 col-centered">
-
-                <ui-view></ui-view>
-
-            </div>
-        </div>
-    </div>
+    <!-- Insert Angular templates -->
+    <ui-view></ui-view>
 
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js"></script>
